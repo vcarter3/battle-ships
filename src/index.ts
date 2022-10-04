36935f35af6629d06b1a2b508dcae95bc9c316df
Veronica Carter <121vcarter@gmail.com>
2022-10-04 09:02:15 +0100

    main screen

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,25 @@
-import { game } from "./factories/UI/game";
+import { game, readPlayerInput} from "./factories/UI/game";
 import "./styles/styles.scss";
 
-game();
+const user = readPlayerInput();
+
+const submit = document.querySelector(".details .submit")
+submit.addEventListener("click", () => {
+    
+    if(user.shipsPlaced()){
+        console.log("good to go")
+    }else{
+        console.log(user.shipsPlaced())
+    }
+    console.log(user.activeCoordinates)
+    //return activeCoordinates
+})
+
+const random = document.querySelector(".details .random")
+random.addEventListener("click", () => {
+    console.log("random")
+    game();
+})
+
+
+
